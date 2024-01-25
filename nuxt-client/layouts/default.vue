767d7fd8e4b091757ec851b717ab789f866c4ea4
Xeth Soto <xasoto@up.edu.ph>
2024-01-25 16:12:15 +0800

    initial commit

---
+++ nuxt-client/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <!-- Logo and Hamburger Icon -->
+                <div>
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8">
+                        <path d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                    <img> 
+                </div>
+
+                <!-- Account Name and Three-dots Icon -->
+                <div>
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></svg>
+                </div>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
