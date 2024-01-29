f7caebc143de12743298ff2087546b097ffff03a
Xeth Soto <xasoto@up.edu.ph>
2024-01-29 14:33:14 +0800

    ui: initial creation of courses management page

---
+++ nuxt-client/layouts/curricular-table.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>
+             <slot name="title"/>
+        </h1>
+        <div class="container h-12">
+
+        </div>
+        <div v-if="tableData.length > 0">
+       </div>
+        <div v-else>
+            <p>No Data</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    // const {tableData} = defineProps(['tableData'])
+    const tableData = ref([])
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt-client/pages/courses-management.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h1>Courses Management</h1>
+        <div class="container h-12">
+
+        </div>
+        <div v-if="courses.length > 0">
+            <UTable :rows="courses" :columns="columns" class="bg-white rounded-lg p-4">
+                <template #actions-data>
+                    <UButton color="red" label="Click Here"/>
+                </template>
+
+                <template #title-header class="font-bold">
+                </template>
+            </UTable>
+        </div>
+        <div v-else>
+            <p>Loading...</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const {data: courses} = await useFetch("http://localhost:3001/courses")
+
+    const columns = [
+        {
+            key: 'course_code',
+            label: "Course Code",
+            sortable: true,
+        },
+        {
+            key: 'title',
+            label: "Title",
+            sortable: true,
+            class: 'text-2xl'
+        },
+        {
+            key: 'description',
+            label: "Description",
+            sortable: true
+        },
+        {
+            key: 'units',
+            label: "Units",
+            sortable: true
+        },
+        {
+            key: 'status',
+            label: "Status",
+            sortable: true
+        },
+        {
+            key: 'actions',
+            label: "Action"
+        }
+    ]
+</script>
+
+<style scoped>
+
+</style>
