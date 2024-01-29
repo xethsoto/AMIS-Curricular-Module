3a1192bd87f77f9f21f1e0342b83bbf1da93d9ed
Xeth Soto <xasoto@up.edu.ph>
2024-01-29 14:34:14 +0800

    chore: added dependencies for nuxt ui module

--- nuxt-client/pages/index.vue
+++ nuxt-client/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Hello</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae natus voluptate quos corrupti odit nulla ducimus voluptates consequuntur. Earum nihil ipsam sunt nesciunt modi facilis eligendi at iusto ullam sint, facere cumque corrupti error explicabo laudantium voluptatibus reprehenderit id alias voluptate doloremque quam veniam nobis. At ipsum in dolores. Atque totam corrupti sed tenetur nobis? Praesentium, nostrum at. Eum, fugit? Exercitationem odio sed eveniet mollitia optio consequatur animi, velit nam illo dicta non amet soluta explicabo culpa voluptatem numquam qui impedit ipsum. Modi ipsum quis officia ad nihil perferendis accusantium minima ab commodi magni atque molestiae eveniet quos, nam aliquam consequatur repudiandae aliquid eos tempora repellendus. Non sapiente ut, iusto in eaque totam perspiciatis iure ducimus nostrum? Tenetur repudiandae illo veniam magni ipsa tempore esse laboriosam nulla sed, enim deleniti maiores similique perferendis fuga necessitatibus, soluta ea dolores. Ipsam, minima sed corporis magnam quia neque veritatis consequuntur aliquid. Eaque, quasi? Eligendi nostrum molestias dolor ipsam? Tempora neque quia saepe eum delectus nam sit incidunt, recusandae magnam perferendis labore cupiditate consequuntur deserunt dicta, harum rerum. Illo, quasi reprehenderit rem facere vitae obcaecati ut incidunt quibusdam assumenda neque, repellendus, tempore dolore nostrum necessitatibus nam? Quibusdam eos et dolorem. Aliquam laborum cumque fugiat.</p>
     </div>
 </template>
 
