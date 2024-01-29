5aa6e7bd335c8cd04cab102fa121819d0c740ae4
Xeth Soto <xasoto@up.edu.ph>
2024-01-29 14:31:41 +0800

    feat: added page for viewing curricula table

---
+++ nuxt-client/pages/curriculum-management.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <NuxtLayout name="curricular-table">
+            <template v-slot:title>Curriculum Management</template>
+        </NuxtLayout>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
