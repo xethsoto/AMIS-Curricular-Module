a6cfb9aa84877d0d5ad8c3d2880ed15bf827b721
Xeth Soto <xasoto@up.edu.ph>
2024-02-29 16:49:22 +0800

    ui: implemented an accordion ui for viewing the degree programs requiring specific course

--- nuxt-client/nuxt.config.ts
+++ nuxt-client/nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', 'nuxt-primevue'],
   colorMode: {
     preference: 'light'
   },
+  css: ['primevue/resources/themes/aura-light-green/theme.css']
 })
--- nuxt-client/pages/courses-management/[id].vue
+++ nuxt-client/pages/courses-management/[id].vue
@@ -31,7 +31,7 @@
                             <div v-for="prereq in prereqs">
                                 <ULink
                                     :to="`/courses-management/${prereq.id}`"
-                                    class="text-blue-900 hover:text-blue-300"
+                                    class="nav-link"
                                 >
                                 {{ `${prereq.code}: ${prereq.title}` }}
                                 </ULink>
@@ -67,7 +67,74 @@
                 <!-- Degree Programs Requiring This Course -->
 
                 <p class="font-bold text-lg text-center">Degree Programs Requiring This Course</p>
+                <Accordion v-if="filteredDegProgs" :multiple="true">
+                    <AccordionTab v-for="degProg in filteredDegProgs" :header="degProg.name">
+                        <div v-if="degProg.has_majors" class="grid grid-cols-2 gap-4">
+                            <div v-for="degMajorObj in degProgMajors" class="text-center">
+
+                                <!-- Filters appropriate deg_prog - major relationship -->
+                                <div v-if="degMajorObj.deg_prog_id == degProg.id">
+
+                                    <div v-for="major in filteredMajors">
+                                        <div v-if="major.id == degMajorObj.major_id">
+                                            <p class="font-semibold">{{ major.name }}</p>
+                                        </div>
+                                    </div>
+
+                                    <!-- Fills in the curriculum that requires current course -->
+                                    <div v-for="curriculum in filteredCurricula">
+                                        <div v-if="curriculum.major_id == degMajorObj.major_id" class="flex flex-row justify-around">
+                                            <ULink class="nav-link">{{ curriculum.name }}</ULink>
+                                            <div v-for="obj in currCourse">
+                                                <div v-if="obj.curr_id == curriculum.id" class="font-bold">
+                                                    {{ obj.course_type }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div v-else>
+                            <div v-for="curriculum in filteredCurricula">
+                                <div v-if="curriculum.deg_prog_id == degProg.id" class="flex flex-row justify-around">
+                                    <ULink class="nav-link">{{ curriculum.name }}</ULink>
+                                    <div v-for="obj in currCourse">
+                                        <div v-if="obj.curr_id == curriculum.id" class="font-bold">
+                                            <p>{{ obj.course_type }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </AccordionTab>
+                </Accordion>
+
+                <div v-else>
+                    <p class="text-semibold">No Degree Programs require this course</p>
+                </div>
+
+                <hr class="h-px border-0 bg-black dark:bg-gray-700">
                 
+                <!-- History -->
+                <p class="font-bold text-lg text-center">Course History</p>
+                <div class="timeline-container">
+                    <Timeline :value="events" layout="horizontal" class="w-full md:w-20rem overflow-x-auto" @wheel="handleScroll">
+                        <template #marker="slotProps">
+                            <div class="border-2 border-solid border-black">
+                                <div v-for="testing in slotProps">
+                                    {{ testing.item }}
+                                </div>
+                            </div>
+                        </template>
+                        <template #content="slotProps">
+                            <div class="container w-96">
+                                
+                            </div>
+                        </template>
+                    </Timeline>
+                </div>
                 
             </template>
         </NuxtLayout>
@@ -75,23 +142,32 @@
 </template>
 
 <script setup>
-    // Functions
-    // const customSort = (array) => {
-    //     console.log("array = ", array)
-    //     array.sort((a, b) => {
-    //         const numA = parseInt(a.match(/\d+/));
-    //         const numB = parseInt(b.match(/\d+/));
-
-    //         // If both strings have numbers, compare them numerically
-    //         if (!isNaN(numA) && !isNaN(numB)) {
-    //             return numA - numB;
-    //         }
-
-    //         return a.localeCompare(b);
-    //      });
-    // }
-
     const { id } = useRoute().params
+
+    const events = [
+        {
+            item: "Institution"
+        },
+        {
+            item: "Revision"
+        },
+        {
+            item: "Revision"
+        },
+        {
+            item: "Revision"
+        },
+        {
+            item: "Revision"
+        },
+        {
+            item: "Abolition"
+        },
+   ]
+
+//    const events = [
+//     "Institution", "Revision", "Revision", "Revision", "Revision", "Revision", "Abolition"
+//     ]  
 
     // fetching course
     const courseURI = 'http://localhost:3001/courses/' + id
@@ -120,18 +196,60 @@
                 return Number(course.id) === prereq.prereq_id
             })
         })
-
-    // customSort(prereqs)
     
     const reqURI = 'http://localhost:3001/course_prereq?prereq_id=' + id
     const { data: reqData } = await useFetch(reqURI)
     const reqs = courses.value.filter((course) => {
             return reqData.value.some((req) => {
                 return Number(course.id) === req.course_id
-            })
         })
+    })
 
-    // customSort(reqs)
+    const { data: curricula } = await useFetch('http://localhost:3001/curriculum')
+    const { data: degProgs } = await useFetch('http://localhost:3001/deg_prog')
+    const { data: majors } = await useFetch('http://localhost:3001/majors')
+    const { data: degProgMajors } = await useFetch('http://localhost:3001/deg_prog_majors')
+    
+    const currCourseURI = 'http://localhost:3001/curr_course?course_id=' + id
+    const { data: currCourse } = await useFetch(currCourseURI)
+
+    // curricula that require said courses
+    const curriculaIds = [...new Set(currCourse.value.map(obj => obj.curr_id))] // eliminates duplicate values
+    const filteredCurricula = curricula.value.filter((curriculum) => {
+        return curriculaIds.includes(Number(curriculum.id))
+    })
+
+    // degree programs of the curriculum that requires the course
+    const degProgIds = [...new Set(filteredCurricula.map(obj => obj.deg_prog_id))]
+    const filteredDegProgs = degProgs.value.filter((degProg) => {
+        return degProgIds.includes(Number(degProg.id))
+    })
+
+    // majors that require specific course
+    const majorsIds = [...new Set(filteredCurricula.map(obj => obj.major_id))]
+    const filteredMajors = majors.value.filter((major) => {
+        return majorsIds.includes(Number(major.id))
+    })
+    
+    // Timeline
+    const scrollableElement = ref(null);
+
+    const handleScroll = ((event) => {
+        // Calculate the horizontal scroll amount based on the deltaX property
+        const delta = event.deltaX || event.deltaY;
+        // Adjust the scrollLeft property of the scrollable element
+        event.currentTarget.scrollLeft += delta;
+
+        if (Math.abs(event.deltaX) > Math.abs(event.deltaY)) {
+            event.preventDefault();
+            // Prevent the window from scrolling
+            window.scrollTo({
+            left: window.scrollX,
+            top: window.scrollY,
+            behavior: 'instant'
+            });
+        }
+    })
 
 </script>
 
