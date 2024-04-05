e3f91b4cfeca99e51209ab802ad6952a33db2425
Xeth Soto <xasoto@up.edu.ph>
2024-04-05 20:53:12 +0800

    chore: added new routes for getting all courses

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProposalsControllers\CourseControllers\CourseInstitutionController;
@@ -20,5 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Courses
+Route::get('get-courses', [CourseController::class, 'getCourses']);
+
+//Proposals
 Route::post('save-proposal', [ProposalController::class, 'save']);
 Route::post('test-proposal', [ProposalController::class, 'test']);
