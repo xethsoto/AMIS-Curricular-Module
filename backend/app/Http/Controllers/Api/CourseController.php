a07df491eaae76ca5379b108f5ce5e8b66455ba4
Xeth Soto <xasoto@up.edu.ph>
2024-04-09 19:06:58 +0800

    feat: added getCourses method for getting a specific course

--- backend/app/Http/Controllers/Api/CourseController.php
+++ backend/app/Http/Controllers/Api/CourseController.php
@@ -2,25 +2,53 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Models\Course\Course;
+use App\Http\Controllers\Controller;
 use App\Models\Course\CoursePrereqs;
 use App\Models\Course\CourseSemOffered;
 
 class CourseController extends Controller
 {
+
+    //used to extract prereq_code and sem_offering
+    //from separate table
+    private function extractInfo($course)
+    {
+        $courseArray = $course->toArray();
+        $courseArray['prereqs'] = $course->getPrereqCode();
+        $courseArray['sem_offered'] = $course->getSemOffered();
+
+        return $courseArray;
+    }
+
     public function getCourses()
     {
         $courses = Course::with('prereqs', 'semOffered')->get();
 
         //only returns prereq_code and sem_offered in prereqs and sem_offered
         return response()->json($courses->map(function ($course) {
-            $courseArray = $course->toArray();
-            $courseArray['prereqs'] = $course->getPrereqCode();
-            $courseArray['sem_offered'] = $course->getSemOffered();
-
-            error_log('$courseArray = '.print_r($courseArray,true));
-            return $courseArray;
+            return $this->extractInfo($course);
         }));
+    }
+
+    public function getRequisites(Request $request)
+    {
+        // 
+    }
+
+    public function getCourse($id)
+    {
+        $course = Course::find($id);
+
+        if ($course) {
+            return response()->json($course);
+        } else {
+            return response()->json([
+                'success' => false,
+                'status' => 404,
+                'message' => 'Course not found' 
+            ], 404);
+        }
     }
 }
--- backend/app/Models/Course/Course.php
+++ backend/app/Models/Course/Course.php
@@ -2,8 +2,8 @@
 
 namespace App\Models\Course;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
