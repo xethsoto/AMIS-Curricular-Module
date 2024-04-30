4b4f1f5266df69b2e502cc7455d84ac2c921b7ab
Xeth Soto <xasoto@up.edu.ph>
2024-04-30 21:18:51 +0800

    feat: added forms for changing course content and number of hours

---
+++ nuxt-client/components/Form/CourseRevision/Content.vue
@@ -0,0 +1,42 @@
+<template>
+    <NuxtLayout name="course-revision" 
+        @selectedCourse="formContent.selectedCourse=$event"
+        @rationale="formContent.rationale=$event"
+    >
+        <template #subtype>
+            Change in course content
+        </template>
+        <template #main-fields>
+            <!-- Course Description -->
+            <FormInput 
+                type="text-area" 
+                label="New Course Goal" 
+                @input="formContent.newGoal = $event"
+            />
+
+            <FormInput 
+                type="text-area" 
+                label="New Course Outline" 
+                @input="formContent.newOutline = $event"
+            />
+        </template>
+    </NuxtLayout>
+</template>
+
+<script setup>
+    const emit = defineEmits(['inputValue'])
+
+    const formContent = reactive({
+        selectedCourse: "",
+        newGoal: "",
+        newOutline: "",
+        rationale: ""
+    })
+
+    watchEffect(() => {
+        emit('inputValue', formContent)
+    })
+</script>
+
+<style scoped>
+</style>
---
+++ nuxt-client/components/Form/CourseRevision/NumOfHours.vue
@@ -0,0 +1,34 @@
+<template>
+    <NuxtLayout name="course-revision" 
+        @selectedCourse="formContent.selectedCourse=$event"
+        @rationale="formContent.rationale=$event"
+    >
+        <template #subtype>
+            Change in number of hours
+        </template>
+        <template #main-fields>
+            <FormInput 
+                type="text-field" 
+                label="New Number of Hours" 
+                @input="formContent.newNumOfHours = $event"
+            />
+        </template>
+    </NuxtLayout>
+</template>
+
+<script setup>
+    const emit = defineEmits(['inputValue'])
+
+    const formContent = reactive({
+        selectedCourse: "",
+        newNumOfHours: "",
+        rationale: ""
+    })
+
+    watchEffect(() => {
+        emit('inputValue', formContent)
+    })
+</script>
+
+<style scoped>
+</style>
