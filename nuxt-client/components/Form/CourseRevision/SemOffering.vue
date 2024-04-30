a9e1f53e94e947f323a39361678bca6f0680afb1
Xeth Soto <xasoto@up.edu.ph>
2024-04-30 14:37:08 +0800

    feat: added feature for editing when the course is going to be offered

---
+++ nuxt-client/components/Form/CourseRevision/SemOffering.vue
@@ -0,0 +1,35 @@
+<template>
+    <p class="font-bold text-center text-lg text-black">Course Revision</p>
+    <p class="font-bold text-center text-base text-black">Change in course description</p>
+
+    <CourseToEdit @input="formContent.selectedCourse=$event"/>
+
+    <label>New Semester Offering</label>
+    <SemOffering @input="formContent.newSemOffering = $event"/>  
+
+    <!-- Rationale -->
+    <FormInput
+        type="text-area"
+        label="Rationale"
+        @input="formContent.rationale = $event"
+    />
+</template>
+
+<script setup>
+    const emit = defineEmits(['inputValue'])
+
+    const formContent = reactive({
+        selectedCourse: "",
+        newSemOffering: "",
+        rationale: ""
+    })
+
+    const searchLabel = "Filter by Course Code"
+
+    watchEffect(() => {
+        emit('inputValue', formContent)
+    })
+</script>
+
+<style scoped>
+</style>
