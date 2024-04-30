0a764d90a5bcd1905dc986eb8df0c7cb6dc88bd1
Xeth Soto <xasoto@up.edu.ph>
2024-04-30 16:08:01 +0800

    feat: created a layout for course revision forms

---
+++ nuxt-client/layouts/course-revision.vue
@@ -0,0 +1,33 @@
+<template>
+    <p class="font-bold text-center text-lg text-black">Course Revision</p>
+    <p class="font-bold text-center text-base text-black">
+        <slot name="subtype"></slot>    <!-- Subtype of course revision -->
+    </p>
+        
+    <CourseToEdit @input="selectedCourse=$event"/>
+
+    <!-- Main Fields for Revisions -->
+    <slot name="main-fields"></slot>
+
+    <!-- Rationale -->
+    <FormInput 
+        type="text-area"
+        label="Rationale"
+        @input="rationale=$event"
+    />
+</template>
+
+<script setup>
+    const emit = defineEmits(['selectedCourse', 'rationale'])
+
+    const selectedCourse = ref("")
+    const rationale = ref("")
+
+    watchEffect(() => {
+        emit('selectedCourse', selectedCourse.value)
+        emit('rationale', rationale.value)
+    })
+</script>
+
+<style scoped>
+</style>
