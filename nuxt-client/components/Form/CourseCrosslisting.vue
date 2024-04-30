36c2d7ee448e2533c2a7ae507b95de404bd79fef
Xeth Soto <xasoto@up.edu.ph>
2024-04-30 21:19:53 +0800

    feat: added form for crosslisting courses

---
+++ nuxt-client/components/Form/CourseCrosslisting.vue
@@ -0,0 +1,33 @@
+<template>
+    <p class="font-bold text-center text-lg text-black">Course Crosslisting</p>
+    <SingleCourseSelect 
+        inputFieldLabel="Course to Edit" 
+        @input="formContent.selectedCourse=$event"
+    />
+    <SingleCourseSelect
+        inputFieldLabel="Course to be crosslisted" 
+        @input="formContent.crosslistCourse = $event"
+    />
+    <FormInput 
+        type="text-area"
+        label="Rationale"
+        @input="formContent.rationale=$event"
+    />
+</template>
+
+<script setup>
+    const emit = defineEmits(['inputValue'])
+
+    const formContent = reactive({
+        selectedCourse: "",
+        crosslistCourse: "",
+        rationale: ""
+    })
+
+    watchEffect(() => {
+        emit('inputValue', formContent)
+    })
+</script>
+
+<style scoped>
+</style>
