3c10ad1adeb2678b5f65fce7e5686107ab34f2a4
Xeth Soto <xasoto@up.edu.ph>
2024-06-15 12:38:56 +0800

    ui: added margin at the bottom of the page

--- nuxt-client/pages/proposals-management/[id].vue
+++ nuxt-client/pages/proposals-management/[id].vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col gap-5">
+    <div class="flex flex-col gap-5 mb-8">
         <!-- Title -->
         <div class="flex flex-row gap-4">
             <NuxtLink to="/proposals-management">
