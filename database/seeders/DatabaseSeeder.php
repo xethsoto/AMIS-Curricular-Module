27614fed53a388544ec0735740f8568549119ac9
Xeth Soto <xasoto@up.edu.ph>
2024-06-28 14:51:49 +0800

    feat: added admin users on database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(2)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'admin1',
+            'email' => 'admin1@email.com',
+            'password' => Hash::make('admin1Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin2',
+            'email' => 'admin2@email.com',
+            'password' => Hash::make('admin2Passcode'),
+            'type' => 'admin',
+        ]);
     }
 }
