341efd0b117d92810f85c6b56bc1716809a7eda9
Xeth Soto <xasoto@up.edu.ph>
2024-06-30 17:20:39 +0800

    chore: added new admins

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,5 +29,63 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('admin2Passcode'),
             'type' => 'admin',
         ]);
+
+        User::factory()->create([
+            'name' => 'admin3',
+            'email' => 'admin3@email.com',
+            'password' => Hash::make('admin3Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin4',
+            'email' => 'admin4@email.com',
+            'password' => Hash::make('admin4Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin5',
+            'email' => 'admin5@email.com',
+            'password' => Hash::make('admin5Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin6',
+            'email' => 'admin6@email.com',
+            'password' => Hash::make('admin6Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin7',
+            'email' => 'admin7@email.com',
+            'password' => Hash::make('admin7Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin8',
+            'email' => 'admin8@email.com',
+            'password' => Hash::make('admin8Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin9',
+            'email' => 'admin9@email.com',
+            'password' => Hash::make('admin9Passcode'),
+            'type' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin10',
+            'email' => 'admin10@email.com',
+            'password' => Hash::make('admin10Passcode'),
+            'type' => 'admin',
+        ]);
+
+
     }
 }
